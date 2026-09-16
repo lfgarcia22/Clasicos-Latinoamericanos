377b6d11a37b542bc96bb93e4ce8b4984d07e486
Luis García Castro <luis.fernando.garcia.castro@gmail.com>
2026-09-16 01:23:05 -0400

    Apply player walking animation

--- Assets/Kits/Common/Scripts/InputActionMove.cs
+++ Assets/Kits/Common/Scripts/InputActionMove.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem.OnScreen;
 public class InputActionMove : MonoBehaviour
 {
 
+  [SerializeField] private GameObject playerWrapper;
   [SerializeField] private GameObject player;
 
   [SerializeField] private float walkSpeed = 5;
@@ -16,12 +17,14 @@ public class InputActionMove : MonoBehaviour
   private OnScreenStick childStickController;
   private InputAction moveAction;
   private Vector3 moveVector;
+  private Animator animator;
 
 
   private void Awake()
   {
     moveAction = InputSystem.actions.FindAction("Move");
     childStickController = GetComponentInChildren<OnScreenStick>();
+    animator = playerWrapper?.GetComponentInChildren<Animator>();
   }
 
 
@@ -29,15 +32,25 @@ public class InputActionMove : MonoBehaviour
   {
     var moveControllerVector = moveAction.ReadValue<Vector2>();
     moveVector = new Vector3(moveControllerVector.x, 0.0f, moveControllerVector.y);
-
   }
 
   private void FixedUpdate()
   {
-    if (allowToMove && player != null)
+    if (allowToMove && playerWrapper != null)
     {
-      player.transform.Translate(Vector3.forward * moveVector.z * walkSpeed * Time.deltaTime, Space.World);
-      player.transform.Translate(Vector3.right * moveVector.x * walkSpeed * Time.deltaTime, Space.World);
+      playerWrapper.transform.Translate(Vector3.forward * moveVector.z * walkSpeed * Time.deltaTime, Space.World);
+      playerWrapper.transform.Translate(Vector3.right * moveVector.x * walkSpeed * Time.deltaTime, Space.World);
+    }
+
+    var isMoving = moveVector.x != 0 || moveVector.z != 0;
+    animator.SetBool("IsWalking", isMoving);
+    if (isMoving)
+    {
+      player.transform.LookAt(new Vector3(
+        playerWrapper.transform.localPosition.x + (moveVector.x * 4),
+        moveVector.y,
+        playerWrapper.transform.localPosition.z + (moveVector.z * 4)
+      ));
     }
   }
 
